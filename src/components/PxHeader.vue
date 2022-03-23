3ff9350701a00540ef70543d9a62085036202ffc
Leidy-Lesmes <70681138+Leidy-Lesmes@users.noreply.github.com>
2022-03-23 02:07:25 -0500

    comprobacion

--- src/components/PxHeader.vue
+++ src/components/PxHeader.vue
@@ -5,7 +5,7 @@
         <div class="flex items-center flex-shrink-0 text-white mr-6">
           <px-icon class="mr-4" />
           <span class="font-semibold text-xl tracking-tight"
-            >ClasificaDogs</span
+            >ClasificaDogs!</span
           >
         </div>
         <div
