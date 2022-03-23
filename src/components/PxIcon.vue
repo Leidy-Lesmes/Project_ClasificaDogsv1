4839b09cf9155e8d008c968ea4c07b4e63facef3
Leidy-Lesmes <70681138+Leidy-Lesmes@users.noreply.github.com>
2022-03-23 02:02:29 -0500

    Inicio

---
+++ src/components/PxIcon.vue
@@ -0,0 +1,23 @@
+<template>
+  <svg
+    version="1.1"
+    id="Layer_1"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+    x="0px"
+    y="0px"
+    width="90px"
+    viewBox="0 0 937 853"
+    enable-background="new 0 0 937 853"
+    xml:space="preserve"
+  >
+    <image
+      id="image0"
+      width="980"
+      height="1000"
+      x="0"
+      y="0"
+      xlink:href="https://th.bing.com/th/id/R.9dd6f9314b259b69873fd391749746ea?rik=V8fe5Y9%2f0E1sYA&pid=ImgRaw&r=0"
+    />
+  </svg>
+</template>
